dc248c56f714eecbec9012a7d99cdb0c9db9ffb8
Ulises101199 <Ulisespg119@gmail.com>
2021-05-28 15:25:45 -0500

    first commit

---
+++ Actividad_3_Ulises/Actividad_3.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+
+var a : Int
+a = 1
+
+var b : String
+ b = "b"
+
+var c : Float
+c = 1.1
+
+
+var numero = [1,2,3,4,5,6,7]
+
+var diccionario = ["Lun":1,"Mar":2,"Mie":3,"Jue":4,"Vie":5,"Sab":6,"Dom":7]
+
+print (diccionario["Dom"])
